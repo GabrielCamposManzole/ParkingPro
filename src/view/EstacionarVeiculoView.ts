69dc9c6c2a363a649f02e88a8cbdaeb1a6bc398f
GabrielCamposManzole <gh987181127@gmail.com>
2025-09-22 17:38:07 -0300

    .

--- src/view/EstacionarVeiculoView.ts
+++ src/view/EstacionarVeiculoView.ts
@@ -4,14 +4,14 @@ import EstacionamentoController from "../control/EstacionamentoController";
 export default class EstacionarVeiculoView {
   private prompt: PromptSync.Prompt;
   private controller: EstacionamentoController;
-  
+
   constructor(controller: EstacionamentoController) {
     this.prompt = PromptSync();
     this.controller = controller;
   }
 
   public fluxoEstacionar(): void {
-    
+
 
 
   }
