28413978704901d18937431eaf01b3398bb83240
GabrielCamposManzole <gh987181127@gmail.com>
2025-11-26 22:37:21 -0300

    Stop tracking dist folder

--- dist/model/ClientType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ClientType = void 0;
-var ClientType;
-(function (ClientType) {
-    ClientType[ClientType["MENSALISTA"] = 1] = "MENSALISTA";
-    ClientType[ClientType["AVULSO"] = 2] = "AVULSO";
-    ClientType[ClientType["ESPECIAL"] = 3] = "ESPECIAL";
-})(ClientType || (exports.ClientType = ClientType = {}));
--- dist/model/TipoVeiculo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TipoVeiculo = void 0;
-var TipoVeiculo;
-(function (TipoVeiculo) {
-    TipoVeiculo["CARRO"] = "carro";
-    TipoVeiculo["MOTO"] = "moto";
-    TipoVeiculo["CAMINHAO"] = "caminhao";
-})(TipoVeiculo || (exports.TipoVeiculo = TipoVeiculo = {}));
